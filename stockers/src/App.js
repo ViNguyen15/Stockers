3a8e3e8e8da392c33ad768f84f898344ec1892bf
ViNguyen15 <fluffywin@gmail.com>
2020-02-08 15:52:23 -0500

    Austin you took too long I just fixed it

--- stockers/src/App.js
+++ stockers/src/App.js
@@ -16,26 +16,12 @@ class App extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        {/* <Ham /> */}
-=======
-        <Ham >
->>>>>>> 2ddb6e005d26a406f428a346b32a0f67f5ec6ddf
+        <ham >
         <hr></hr>
         <Navbar />
         <Tabs></Tabs>
         <hr></hr>
-<<<<<<< HEAD
-
-=======
-        {/* <Fragment>
-        <NavBar />
-        <div className="container-fluid">
-          <Route path="/" exact component={Welcome} />
-        </div>
-      </Fragment> */}
-  </Ham>
->>>>>>> 2ddb6e005d26a406f428a346b32a0f67f5ec6ddf
+      </ham>
         <DashComp></DashComp>
         <hr></hr>
 
