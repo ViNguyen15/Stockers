2fec38a1ac03005e2f809d34fcfc8b88aa53c3af
Ryan Cunico <rcunico@ggc.edu>
2020-02-08 22:20:50 -0500

    Fixed merge

--- stockers/src/App.js
+++ stockers/src/App.js
@@ -1,13 +1,6 @@
 import React, { Component } from "react";
 import {Route} from 'react-router-dom';
-// import NavBar from './components/NavBar';
-// import Welcome from './components/Welcome';
-//import withSplashScreen from "./components/withSplashScreen";
-//import logo from "./logo.svg";
 import DashComp from "./components/dashboardcomp.jsx";
-//import Tabs from "./components/tabs.jsx";
-//import Ham from "./components/ham.jsx";
-//import "./App.css";
 import Logos from "./components/logo.jsx";
 import Tabs from "./components/tabs.jsx";
 import Navbar from "./components/navbar";
@@ -16,7 +9,6 @@ import { Container } from "react-bootstrap";
 
 
 class App extends Component {
-<<<<<<< HEAD
   constructor(props) {
     super(props);
     this.state = {
@@ -39,27 +31,9 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-=======
-  
-  render() {
-    return (
         <div>
->>>>>>> 2448f7bd3280240ab5700a3efa39e320de1d05f5
         <Navbar />
         </div>
-<<<<<<< HEAD
-      </Fragment> */}
-
-        <DashComp></DashComp>
-        <hr></hr>
-
-        <Logos>hi</Logos>
-        <hr></hr>
-      </div>
-=======
-  
->>>>>>> 2448f7bd3280240ab5700a3efa39e320de1d05f5
     );
   }
 }
